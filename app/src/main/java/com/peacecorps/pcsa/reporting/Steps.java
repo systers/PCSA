34cdfbd828701062cbb05c63f319ce27176d0895
Prashant Solanki <solankisrp2@gmail.com>
2016-03-25 00:22:12 +0530

    Updated Reporting Steps Activity.

--- app/src/main/java/com/peacecorps/pcsa/reporting/Steps.java
+++ app/src/main/java/com/peacecorps/pcsa/reporting/Steps.java
@@ -1,13 +1,25 @@
 package com.peacecorps.pcsa.reporting;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.peacecorps.pcsa.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * @author Buddhiprabha Erabadda
@@ -16,33 +28,74 @@ import com.peacecorps.pcsa.R;
  */
 public class Steps extends Activity {
 
-    TextView reporting_step1;
-    TextView reporting_step2;
-    TextView reporting_step3;
-    TextView reporting_step4;
-    TextView reporting_step5;
-    TextView reporting_step6;
+    @Bind(R.id.steps_recycler)
+    RecyclerView recyclerView;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporting_steps);
+        ButterKnife.bind(this);
+        toolbar.setTitle(getResources().getString(R.string.reporting_steps));
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        List<Spanned> strings =  new ArrayList<>();
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step1)));
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step2)));
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step3)));
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step4)));
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step5)));
+        strings.add(Html.fromHtml(getResources().getString(R.string.reporting_step6)));
+        StepsAdapter stepsAdapter = new StepsAdapter(strings,this);
+        recyclerView.setAdapter(stepsAdapter);
 
-        reporting_step1 = (TextView) findViewById(R.id.reporting_step1);
-        reporting_step2 = (TextView) findViewById(R.id.reporting_step2);
-        reporting_step3 = (TextView) findViewById(R.id.reporting_step3);
-        reporting_step4 = (TextView) findViewById(R.id.reporting_step4);
-        reporting_step5 = (TextView) findViewById(R.id.reporting_step5);
-        reporting_step6 = (TextView) findViewById(R.id.reporting_step6);
+    }
 
-        reporting_step1.setText(Html.fromHtml(getResources().getString(R.string.reporting_step1)));
-        reporting_step2.setText(Html.fromHtml(getResources().getString(R.string.reporting_step2)));
-        reporting_step3.setText(Html.fromHtml(getResources().getString(R.string.reporting_step3)));
-        reporting_step4.setText(Html.fromHtml(getResources().getString(R.string.reporting_step4)));
-        reporting_step5.setText(Html.fromHtml(getResources().getString(R.string.reporting_step5)));
-        reporting_step6.setText(Html.fromHtml(getResources().getString(R.string.reporting_step6)));
+    class StepsAdapter extends RecyclerView.Adapter<StepsVh>{
 
+        List<Spanned> steps;
+        Context context;
+
+        public StepsAdapter(List<Spanned> steps, Context context) {
+            this.steps = steps;
+            this.context = context;
+        }
+
+        @Override
+        public StepsVh onCreateViewHolder(ViewGroup parent, int viewType) {
+            final ViewGroup view = (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(R.layout.steps_recycler_item, parent, false);
+            return new StepsVh(view);
+        }
+
+        @Override
+        public void onBindViewHolder(StepsVh holder, int position) {
+            holder.setData(steps.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return steps.size();
+        }
+    }
+
+    class StepsVh extends RecyclerView.ViewHolder{
+
+        @Bind(R.id.tv)
+        TextView textView;
+
+        public StepsVh(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+
+        void setData(Spanned s){
+            textView.setText(s);
+        }
     }
 
 }
