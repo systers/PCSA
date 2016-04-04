a8091f526c54e1fb64db4ba9f0e9ed2d4de7a65d
Minerva Panda <minervapandaniki@gmail.com>
2016-04-05 01:52:51 +0530

    Update SafetyResources.java
    Code comment to method and class

--- app/src/main/java/com/peacecorps/pcsa/safety_resources/SafetyResources.java
+++ app/src/main/java/com/peacecorps/pcsa/safety_resources/SafetyResources.java
@@ -14,7 +14,9 @@ import com.peacecorps.pcsa.R;
  * @since 2015-08-18
  */
 public class SafetyResources extends AppCompatActivity {
-
+/** 
+ * Called when the activity is first created and load the layout specified in the activity_safety_resources.xml
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
